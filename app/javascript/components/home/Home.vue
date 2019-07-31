bbec4c9892a606e3f15b17fedfa231e18ed53fb8
youknowcast <a1902ha@aiit.ac.jp>
2019-07-31 10:52:18 +0000

    show list on window

--- app/javascript/components/home/Home.vue
+++ app/javascript/components/home/Home.vue
@@ -1,27 +1,65 @@
 <template>
   <div id="home">
-  <v-data-table
-    :headers="headers"
-    :items="pokemons"
-    :items-per-page="5"
-    class="elevation-1"
-  ></v-data-table>
+    <div class="content">
+ 
+    <ul>
+      <li v-for="pokemon in pokemons">
+        {{pokemon.name}}
+        
+      </li>
+      </ul>
+    </div>
   </div>
 
 </template>
 
 
 <script>
+  var axios = require('axios')
   export default{
-    data(){
+    data() {
       return {
-        pokemons:[]
+        loading: true,
+        totalPokemons: 0,
+        headers: [
+          {
+            text: 'なまえ',
+            value: 'name',
+          },
+          {text: 'test',
+          value: 'num'}
+        ],
+        //pokemons:[{num: 5, name: 'piyo'},{num: 5, name: 'piyo'},{num: 5, name: 'piyo'},{num: 5, name: 'piyo'},{num: 5, name: 'piyo'},{num: 5, name: 'hoge'}, {num: 5, name: 'huga'}, {num: 5, name: 'piyo'},]
+        pokemons: []
       };
     },
-    created(){
-      axios.get('https://qiita.com/api/v2/items?page=1&per_page=20').then(function (res){
-        this.pokemons = res.data;
+    watch: {
+      options: {
+        handler () {
+          axios.get('pokemons/').then(function (res){
+            console.log(res.data.list.monsters);
+            self.pokemons = res.data.list.monsters;
+          });
+        },
+        deep: true,
+      },
+    },
+    mounted() {
+      const self = this;
+      axios.get('pokemons/').then(function (res){
+        console.log(res.data.list.monsters);
+        self.pokemons = res.data.list.monsters;
       });
     },
+//    created(){
+//      axios.get('pokemons/').then(function (res){
+//        console.log(res.data.list.monsters);
+//        self.pokemons = res.data.list.monsters;
+//        self.pokemons = [
+//        {name: 'hogehoge'},
+//        {name: 'hugahuga'},
+//        ]
+//      });
+//    },
   }
 </script>
